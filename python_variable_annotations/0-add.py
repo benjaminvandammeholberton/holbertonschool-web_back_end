9b5319b4e7384179fdd7a380dcbe0e7dbb0a2481
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-08-28 16:42:05 +0200

    Add: task 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+This is a function called 'add' that takes two float numbers as
+input and returns their sum as a float.
+
+Example Usage:
+result = add(3.5, 2.7)
+print(result)  # Output: 6.2
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    This function takes two float numbers as input and returns their
+    sum as a float.
+
+    Parameters:
+    a (float): The first float number.
+    b (float): The second float number.
+
+    Returns:
+    float: The sum of the two input numbers.
+    """
+    return a + b
