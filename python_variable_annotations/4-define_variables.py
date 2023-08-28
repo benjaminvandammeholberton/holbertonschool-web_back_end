2618ee8e253acbf2b9770a2aeece6931927ac488
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-08-28 17:15:44 +0200

    Add: task 4

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This code snippet initializes four variables: `a`, `pi`,
+`i_understand_annotations`, and `school`.
+The variables are assigned with specific data types and values.
+
+Example Usage:
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
