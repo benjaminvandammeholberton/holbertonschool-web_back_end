9e935e4c6eb140508859897d582da6f56e85c9be
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-08-28 16:48:38 +0200

    Add: task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This code snippet defines a function called `concat`
+that takes two string arguments `str1` and `str2` and
+returns their concatenation.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the result.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenation of str1 and str2.
+    """
+    return str1 + str2
