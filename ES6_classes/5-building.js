cd58551b2bdadf37dfe66a62aa04b2661526ecde
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-09-14 13:32:44 +0200

    Fix: task 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,20 +1,15 @@
 export default class Building {
   constructor(sqft) {
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft nust be a number');
-    }
-    this._sqft = sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
