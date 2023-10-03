fb846dedb52dca77cfb5176f09f6df59d22be196
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-10-03 12:21:29 +0200

    Fix: task 0

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,3 +1,24 @@
-export default function getResponseFromAPI() {
-  return new Promise(function (resolve, reject) {});
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // You can put your API call logic here, for example:
+    // fetch('https://api.example.com/data')
+    //   .then(response => {
+    //     if (!response.ok) {
+    //       throw new Error('Network response was not ok');
+    //     }
+    //     return response.json();
+    //   })
+    //   .then(data => resolve(data))
+    //   .catch(error => reject(error));
+
+    // For demonstration purposes, let's simulate an error.
+    const simulateError = false;
+    if (simulateError) {
+      reject(new Error('Simulated error'));
+    } else {
+      resolve('Response from API');
+    }
+  });
 }
+
+export default getResponseFromAPI;
