953c469d047a17c094dd54f552d6ce5d76150266
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-10-05 13:42:21 +0200

    Add: task 2

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,11 @@
+function handleResponseFromAPI(promise) {
+  return promise
+    .then((response) => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'Success' };
+    })
+    .catch((error) => {
+      return error;
+    });
+}
+export default handleResponseFromAPI;
