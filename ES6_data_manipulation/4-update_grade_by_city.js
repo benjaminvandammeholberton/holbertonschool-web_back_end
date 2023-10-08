f3219296ed26ee98f0fe770de2d8bd58af7e3646
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-10-08 10:30:56 +0200

    Add: task 4

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,28 @@
+function updateStudentGradeByCity(students, city, newGrades) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  if (typeof city !== 'string') {
+    throw new Error('city must be a string');
+  }
+
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const matchingGrade = newGrades.find(
+        (grade) => grade.studentId === student.id
+      );
+
+      const updatedStudent = { ...student };
+
+      if (matchingGrade) {
+        updatedStudent.grade = matchingGrade.grade;
+      } else {
+        updatedStudent.grade = 'N/A';
+      }
+
+      return updatedStudent;
+    });
+}
+
+module.exports = updateStudentGradeByCity;
