8b92c6e46618a520d02563c1403a3121289e7b4b
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-05 13:36:36 +0100

    add: task 10

---
+++ python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module defines safe_first_element function
+"""
+from typing import Sequence, Any, Optional
+def safe_first_element(lst: Sequence[Any])->Optional[Any]:
+    """
+    Function that returns the first element of a sequence
+
+    Args:
+        lst: the sequence we need to extract the first element
+    
+    Returns: The first element if the argument exists, otherwise None
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
