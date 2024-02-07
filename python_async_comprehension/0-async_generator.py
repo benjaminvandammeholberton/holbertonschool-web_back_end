76358bc016e9c277ce652d14ed6f3a4c2cc14c24
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-07 12:17:49 +0100

    fix: task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,6 +7,6 @@ from typing import AsyncGenerator
 
 
 async def  async_generator() -> AsyncGenerator[float, None]:
-    for i in range(0, 11):
+    for i in range(0, 10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
