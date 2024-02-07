8f09387bace456563e1a576111adc4fb4505f63d
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-07 12:54:35 +0100

    Fix: task 2

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -12,7 +12,7 @@ async def measure_runtime() -> float:
     Coroutine that executes "async_comprehension" four times in parallel
     using "asyncio.gather"
     """
-    tasks = [asyncio.create_task(async_comprehension()) for i in range (0, 4)]
+    tasks = [asyncio.create_task(async_comprehension()) for i in range(0, 4)]
     start = time.perf_counter()
     await asyncio.gather(*tasks)
     end = time.perf_counter()
