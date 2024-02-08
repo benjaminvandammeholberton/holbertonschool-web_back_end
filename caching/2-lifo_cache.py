08299f8ce4b39d7085ba37db995328bb89df8ed3
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-08 11:47:03 +0100

    add: task 2

---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" LIFO Caching module
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ A Lifo caching implementation
+    """
+    def __init__(selft):
+        """Initialize
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Assign an item to the cache
+            if the cache is full, the last item is removed
+        """
+        if key and item:
+            if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                last_item = list(self.cache_data)[-1]
+                self.cache_data.pop(last_item)
+                print(f"DISCARD: {last_item}")
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key and key in self.cache_data:
+            return self.cache_data[key]
+        return None
