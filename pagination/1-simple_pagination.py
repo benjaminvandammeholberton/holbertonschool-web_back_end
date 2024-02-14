3108f31839e63ae1427652e284dab0d328819c6f
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-14 19:45:22 +0100

    fix: task 1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,29 +1,10 @@
 #!/usr/bin/env python3
-"""
-Simple pagination
-"""
+"""module that handle simple pagination"""
 import csv
 import math
 from typing import List
-from typing import Tuple
 
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """
-    Calculates the starting and ending indices for a given page in a paginated
-    data set.
-
-    Args:
-        page (int): The page number for which to calculate the indices.
-        page_size (int): The number of items per page.
-
-    Returns:
-        tuple: A tuple containing the starting and ending indices.
-    """
-    index_start = page * page_size - page_size
-    index_end = page * page_size
-
-    return (index_start, index_end)
+index_range = __import__('0-simple_helper_function').index_range
 
 
 class Server:
@@ -45,12 +26,10 @@ class Server:
 
         return self.__dataset
 
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Finds the correct indexes to paginate dataset correctly"""
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
 
-def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-    """Finds the correct indexes to paginate dataset correctly"""
-    assert type(page) is int
-    assert page > 0
-    assert type(page_size) is int
-    assert page_size > 0
-    indexes = index_range(page, page_size)
-    return self.dataset()[indexes[0]:indexes[1]]
+        start, end = index_range(page, page_size)
+        return self.dataset()[start:end]
