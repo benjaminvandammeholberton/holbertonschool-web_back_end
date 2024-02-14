6c779e6918f9ff8e077e2263ba4708bea3f9bbb3
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-14 18:11:18 +0100

    fix: task 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" Simple helper function
+""" 
+Simple helper function
 """
 
 
@@ -9,6 +10,6 @@ def index_range(page: int, page_size: int) -> tuple[int, int]:
     corresponding to the range of indexes to return in a list for those
     particular pagination parameters.
     """
-    start_index = page - 1 * page_size
+    start_index = (page - 1) * page_size
     end_index = page_size * page
     return (start_index, end_index)
