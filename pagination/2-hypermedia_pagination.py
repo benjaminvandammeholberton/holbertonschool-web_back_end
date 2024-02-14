122e01e7c295c899ad42827eb266704250ca1b3c
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-14 21:17:03 +0100

    fix: task 2

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -34,7 +34,7 @@ class Server:
         start, end = index_range(page, page_size)
         return self.dataset()[start:end]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict[List]:
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         """Returns a dictionnary containing hypermedia pagination
         """
         assert isinstance(page, int) and isinstance(page_size, int)
