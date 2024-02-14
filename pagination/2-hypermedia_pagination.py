c116f6e91f7d7d8dea8c0c8ca0a9893fcd7aa6a2
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-14 21:13:14 +0100

    add: task 2

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -1,18 +1,10 @@
 #!/usr/bin/env python3
-"""Hypermedia pagination"""
+"""module that handle hypermedia pagination"""
 import csv
 import math
 from typing import List
 
-
-def index_range(page, page_size):
-    """returns a tuple of size two containing a start index and an end
-    index corresponding to the range of indexes to return in a list for
-    those particular pagination parameters."""
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-
-    return start_index, end_index
+index_range = __import__('0-simple_helper_function').index_range
 
 
 class Server:
@@ -42,14 +34,19 @@ class Server:
         start, end = index_range(page, page_size)
         return self.dataset()[start:end]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
-        """Returns a dictionary containing the following key-value pairs"""
-        total_pages = math.ceil(len(self.dataset()) / page_size)
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict[List]:
+        """Returns a dictionnary containing hypermedia pagination
+        """
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+
+        total_page = math.ceil(len(self.dataset()) / page_size)
+
         return {
             'page_size': len(self.get_page(page, page_size)),
             'page': page,
             'data': self.get_page(page, page_size),
-            'next_page': page + 1 if page < total_pages else None,
+            'next_page': page + 1 if page < total_page else None,
             'prev_page': page - 1 if page > 1 else None,
-            'total_pages': total_pages
-        }
+            'total_pages': total_page
+            }
