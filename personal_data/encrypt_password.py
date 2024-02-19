463ce1f9d9a235e35727f353bcb6a97e61e57ea3
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-19 16:34:30 +0100

    add: task 6

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -9,3 +9,10 @@ def hash_password(password: str) -> bytes:
     """Function that hashes a password using bcrypt"""
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Function that verify a password against a hashed password"""
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        return True
+    return False
