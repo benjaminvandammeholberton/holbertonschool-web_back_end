480826f7b976de0a274c29a04bdca8bb6369513d
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-19 12:38:48 +0100

    fix: task 1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -30,6 +30,7 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """Return message formated"""
         default_message = super().format(record)
         message = filter_datum(self.fields, self.REDACTION, default_message,
                                self.SEPARATOR)
