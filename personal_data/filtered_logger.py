c9705883cb312f7610a08f5850ff5052cb478af5
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-19 12:36:46 +0100

    add: task 1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,7 +2,7 @@
 """
 Task 0 - Regex-ing
 """
-
+import logging
 import re
 from typing import List
 
@@ -14,3 +14,23 @@ def filter_datum(fields: List[str], redaction: str,
         pattern = rf"(?<={field}=)[^{separator}]+(?={separator})"
         message = re.sub(pattern, redaction, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        default_message = super().format(record)
+        message = filter_datum(self.fields, self.REDACTION, default_message,
+                               self.SEPARATOR)
+        return message
