f7af24862931c3001ee8a8b3e368cad21118ff38
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-19 11:31:28 +0100

    add: task 0

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/ebnv python3
+"""
+Task 0 - Regex-ing
+"""
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Returns the log message obfuscated"""
+    for field in fields:
+        pattern = rf"(?<={field}=)[^{field}]+(?={separator})"
+        message = re.sub(pattern, redaction, message)
+    return message
