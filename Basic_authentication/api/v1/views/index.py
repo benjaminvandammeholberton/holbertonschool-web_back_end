a9084fa6d0437fdbec73071fdaab96fe1c960087
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-20 11:00:31 +0100

    add: task 2

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -23,6 +23,15 @@ def unauthorized() -> str:
     abort(401, description="Unauthorized")
 
 
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ Get /api/v1/forbidden
+    Return:
+      - error 403
+    """
+    abort(403, description="Forbidden")
+
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
