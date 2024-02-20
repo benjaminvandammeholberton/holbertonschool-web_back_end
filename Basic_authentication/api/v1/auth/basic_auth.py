25fe98ad7108e6e468bbd035ab3ebe84bca19559
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-20 14:57:48 +0100

    add: task 6

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,3 +7,13 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ BasicAuth class
     """
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ function that extract Authorization value from header request
+        """
+        if (authorization_header is None or
+            not isinstance(authorization_header, str) or
+                authorization_header[:6] != 'Basic '):
+            return None
+
+        return authorization_header[6:]
