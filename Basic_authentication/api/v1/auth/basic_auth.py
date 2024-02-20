be451cbd45d2eeac9bd172fd647d54a5eb074dee
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-20 16:22:58 +0100

    add: task 8

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Module for basic auth
 """
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -17,3 +18,19 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """ function that decodes a value of Base64
+        """
+        if (base64_authorization_header is None or
+                not isinstance(base64_authorization_header, str)):
+            return None
+
+        try:
+            decoded_auth = base64.b64decode(base64_authorization_header)
+            return decoded_auth
+
+        except Exception:
+            return None
