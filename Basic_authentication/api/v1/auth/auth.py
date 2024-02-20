c22bc956964f9de2a414a0107cf079387aa782e6
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-20 11:30:33 +0100

    add: task 4

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -13,7 +13,15 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Returns False
         """
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if path[len(path)-1] != '/':
+            path = path + '/'
+
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> TypeVar('User'):
         """ Returns None
