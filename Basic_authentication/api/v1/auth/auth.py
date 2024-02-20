5ab1db5dc2d9e3461aeda9b4384394e6a712e4c0
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-20 11:15:54 +0100

    add: task 3

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,25 @@
+""" Module of authentification
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Authentification class
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Returns False
+        """
+        return False
+
+    def authorization_header(self, request=None) -> TypeVar('User'):
+        """ Returns None
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Returns None
+        """
+        return None
