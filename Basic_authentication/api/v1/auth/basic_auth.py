b1819491dd897e0f886468a66e2b0814fd726ff5
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-20 17:39:39 +0100

    add: task 10

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,9 @@
 """ Module for basic auth
 """
 import base64
+from typing import TypeVar
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -47,3 +49,21 @@ class BasicAuth(Auth):
                 ':' not in decoded_base64_authorization_header):
             return (None, None)
         return tuple(decoded_base64_authorization_header.split(':'))
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str
+            ) -> TypeVar('User'):
+        """ Function that retrieve an user based on the credentials
+        """
+        if (user_email is None or
+                user_pwd is None):
+            return None
+        if User.count() == 0:
+            return None
+        if len(User.search({'email': user_email})) == 0:
+            return None
+        user = User.search({'email': user_email})[0]
+        if user.is_valid_password(user_pwd):
+            return user
