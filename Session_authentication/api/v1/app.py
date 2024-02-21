d8089a8d95ea7372c7377a8ddf8a62af3f00e236
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-21 13:58:14 +0100

    add: task 5

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -39,15 +39,12 @@ def before_request():
                                             '/api/v1/auth_session/login/']):
         return
 
-    if auth.authorization_header(request) is None:
+    if (auth.authorization_header(request) is None and
+            auth.session_cookie(request) is None):
         abort(401)
 
     if auth.current_user(request) is None:
         abort(403)
-
-    if (auth.authorization_header(request) is None and
-            auth.session_cookie(request) is None):
-        abort(401)
 
     request.current_user = auth.current_user(request)
 
