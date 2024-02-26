b5f67ecdc329801be61783f69eb511400e665b1b
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-26 15:11:01 +0100

    add: task 6

---
+++ user_authentication_service/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Entry point
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('GET', "/")
+def test():
+    """ Route to test the server
+    """
+    return Flask.jsonify({"message": "Bienvenue"})
+
+
+if __name__ == '__name__':
+    app.run(host="0.0.0.0", port="5000")
