d3cec58d26cd3f392053c463be3bc9a5a42ff05d
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-26 15:05:19 +0100

    add: task 5

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -2,9 +2,29 @@
 """ Authentificate module
 """
 import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
+
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
     """ Method that hashes password using bcrypt
     """
     return bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registers a new user"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            self._db.add_user(email, _hash_password(password))
