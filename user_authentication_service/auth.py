7aab59c15eb3828e83cc1502100c6dfab2791365
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-27 13:17:24 +0100

    add: task 13

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -72,10 +72,10 @@ class Auth:
             return None
 
     def destroy_session(self, user_id: int) -> None:
-        """ destroys a user's session
+        """ Destroys a user's session
         """
         try:
-            user = self._db.find_user_by(id=user_id)
             self._db.update_user(user_id, session_id=None)
+            return None
         except Exception:
-            return
+            return None
