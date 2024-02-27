ef747fc22fe7e3249c9f60de9d36f2df084b1b6d
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-27 13:13:50 +0100

    add: task 13

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -70,3 +70,9 @@ class Auth:
 
         except Exception:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """ destroy a user's session
+        """
+        self._db.update_user(user_id, session_id=None)
+        return None
