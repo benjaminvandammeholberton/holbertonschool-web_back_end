9d622ce96569c24dda586371a0525977bcbe86f0
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-28 10:13:19 +0100

    fix: task 16

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -82,15 +82,15 @@ class Auth:
             raise
 
     def get_reset_password_token(self, email: str) -> str:
-        """
-        Generates a UUID and updates
-        the users' reset_token database field
+        """ Generates reset password token
         """
         try:
             user = self._db.find_user_by(email=email)
-            user_id = user.id
-            reset_token = _generate_uuid()
-            self._db.update_user(user_id, reset_token=reset_token)
-            return reset_token
+            if not user:
+                raise ValueError
+            token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=token)
+            return token
+
         except Exception:
             raise ValueError
