e3ae15b628d0d7d4c1c0c3c19ef0d3b95a31e954
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-28 11:01:23 +0100

    fix: task 19

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -87,7 +87,7 @@ def get_reset_password_token() -> str:
         abort(403)
 
 
-@app.route('/reset_password', methofs=['PUT'])
+@app.route('/reset_password', methods=['PUT'])
 def reset_password():
     """ Route to update the password
     """
