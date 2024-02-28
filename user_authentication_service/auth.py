5755f958048c4d0cea88605b98d53b2f18b3f55f
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-28 10:00:39 +0100

    fix: task 16

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -86,7 +86,7 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            token = str(uuid4())
+            token = _generate_uuid
             self._db.update_user(user.id, reset_token=token)
             return token
 
