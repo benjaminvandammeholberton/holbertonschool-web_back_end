358fb012f75da0cc2f185a9969af7674db9bb458
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-03-04 15:00:11 +0100

    fix: task 2

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -34,16 +34,23 @@ class TestAccessNestedMap(unittest.TestCase):
 
 
 class TestGetJson(unittest.TestCase):
-    """ Unit tests class for get_json function
-    """
-    @patch('requests.get')
-    def test_get_json(self, mock_requests_get):
-        """ Tests return values of get_json function
-        """
-        mock_response = mock_requests_get.return_value
-        mock_response.json.return_value = {"payload": True}
-        response = get_json("http://example.com")
-        self.assertEqual(response, {"payload": True})
-        mock_response.json.return_value = {"payload": False}
-        response = get_json("http://holberton.io")
-        self.assertEqual(response, {"payload": False})
+    """TestGetJson class"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url: str, test_payload: Dict) -> None:
+        """Tests the get_json function"""
+        patcher = patch("requests.get")
+        mock_get = patcher.start()
+
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+
+        mock_get.return_value = mock_response
+
+        self.assertIsInstance(mock_get.return_value, Mock)
+        self.assertEqual(get_json(test_url), test_payload)
+
+        patcher.stop()
