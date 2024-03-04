4f992b1dca1eef5ea4e0690e70a19ec2d422b8aa
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-03-04 15:41:02 +0100

    fix: task 2

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -40,17 +40,18 @@ class TestGetJson(unittest.TestCase):
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False})
     ])
-    def test_get_json(self, test_url: str, test_payload: Dict) -> None:
+    @patch("requests.get")
+    def test_get_json(
+            self,
+            test_url: str,
+            test_payload: Dict,
+            mock_get
+    ) -> None:
         """Tests the get_json function"""
-        patcher = patch("requests.get")
-        mock_get = patcher.start()
 
         mock_response = Mock()
         mock_response.json.return_value = test_payload
 
         mock_get.return_value = mock_response
 
-        self.assertIsInstance(mock_get.return_value, Mock)
         self.assertEqual(get_json(test_url), test_payload)
-
-        patcher.stop()
