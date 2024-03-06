42680ef976f7c16aa53f7dd8f9999a1666057434
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-03-06 09:35:50 +0100

    fix: task 1

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -5,15 +5,19 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
+app = Flask(__name__)
+babel = Babel(app)
+
 
 class Config:
     """ Config class for Flask app
     """
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app = Flask(__name__)
-babel = Babel(app)
+app.config.from_object(Config)
 
 
 @app.route("/")
