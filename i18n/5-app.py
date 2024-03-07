1ba3bffa31be86705ace3891b403ab62fca5d0f7
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-03-07 14:20:24 +0100

    fix: task 5

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -3,9 +3,9 @@
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
+
 app = Flask(__name__)
 babel = Babel(app)
-
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -13,22 +13,6 @@ users = {
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
-
-
-def get_user() -> dict | None:
-    """Retrieve an user from the param 'login_as'"""
-    if (not request.args['login_as'] or
-            int(request.args['login_as']) not in users):
-        return None
-    return users.get(int(request.args['login_as']))
-
-
-@app.before_request
-def before_request():
-    """set user to g.user"""
-    user = get_user()
-    if user:
-        g.user = user
 
 
 class Config():
@@ -41,12 +25,32 @@ class Config():
 app.config.from_object(Config)
 
 
+def get_user():
+    """Get user information based on user ID"""
+    try:
+        user_id = int(request.args.get("login_as"))
+        if user_id in users.keys():
+            return users[user_id]
+    except Exception:
+        return None
+
+
+@app.before_request
+def before_request():
+    """Set user information in flask.g.user"""
+    user = get_user()
+    if user:
+        g.user = user
+
+
 @babel.localeselector
 def get_locale():
     """Get locale"""
-    if ('locale' in request.args and
-            request.args['locale'] in app.config['LANGUAGES']):
-        return request.args['locale']
+    if 'locale' in request.args:
+        requested_locale = request.args['locale']
+
+        if requested_locale in app.config['LANGUAGES']:
+            return requested_locale
 
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
@@ -54,11 +58,8 @@ def get_locale():
 @app.route('/')
 def index():
     """Return index.html"""
-    username = None
-    if g.user:
-        username = g.user['name']
-    return render_template('5-index.html', username=username)
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
