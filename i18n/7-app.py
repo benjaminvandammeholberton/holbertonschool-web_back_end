f9ece20974c3190359394afbd82d0be84967979e
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-03-07 14:32:09 +0100

    add: task 7

---
+++ i18n/7-app.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+"""Route API
+"""
+from typing import Dict
+from flask import Flask, g, render_template, request
+from flask_babel import Babel
+import pytz
+
+app = Flask(__name__)
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """Config class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """Returns a user dictionary or None based on the ID
+    """
+    lang = app.config['LANGUAGES']
+
+    if 'locale' in request.args and request.args['locale'] in lang:
+        return request.args['locale']
+    try:
+        if g.user['locale'] in lang:
+            return g.user['locale']
+    except Exception:
+        pass
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@babel.timezoneselector
+def get_timezone() -> str:
+    """Returns a URL-provided or user time zone
+    """
+    timezone_url = request.args["timezone"]
+    try:
+        if not timezone_url:
+            timezone = g.user["timezone"]
+        else:
+            timezone = timezone_url
+        return timezone
+    except (pytz.exceptions.UnknownTimeZoneError, Exception):
+        return app.config['BABEL_DEFAULT_TIMEZONE']
+
+
+def get_user() -> Dict:
+    """Finds a user if any, and set it as a global on flask.g.user
+    """
+    try:
+        user_id = int(request.args.get('login_as'))
+        if user_id in users.keys():
+            return users[user_id]
+    except Exception:
+        return None
+
+
+@app.before_request
+def before_request():
+    """Before request
+    """
+    user = get_user()
+    if user:
+        g.user = user
+
+
+@app.route('/')
+def index():
+    """index.html
+    """
+    try:
+        username = g.user['name']
+    except Exception:
+        username = None
+    return render_template('7-index.html', username=username)
+
+
+if __name__ == '__main__':
+    app.run()
