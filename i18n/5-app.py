55c8c99074f507a1134e714436aa7f4010ee0ec9
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-03-07 12:51:41 +0100

    add: task 5

---
+++ i18n/5-app.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""Route module for the API"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> dict | None:
+    """Retrieve an user from the param 'login_as'"""
+    if not request.args['login_as'] or request.args['login_as'] not in users:
+        return None
+    return users.get(request.args['login_as'])
+
+
+@app.before_request
+def before_request():
+    """set user to g.user"""
+    g.user = get_user()
+
+
+class Config():
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """Get locale"""
+    if ('locale' in request.args and
+            request.args['locale'] in app.config['LANGUAGES']):
+        return request.args['locale']
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Return index.html"""
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
