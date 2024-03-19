9e52ff7498b24ea4b59cb810c4d086187acfb2a8
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-03-19 12:25:19 +0100

    fix: task 2

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
 -- Task 2
 
 -- Ranks country origins of bands, ordered by the number of (non-unique) fans
-SELECT origin, COUNT(fans) AS nb_fans FROM metal_bands WHERE nb_fans > 1 GROUP BY origin ORDER BY COUNT(fans) DESC;
+SELECT origin, COUNT(fans) AS nb_fans FROM metal_bands WHERE fans > 1 GROUP BY origin ORDER BY COUNT(fans) DESC;
