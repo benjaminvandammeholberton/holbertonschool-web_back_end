ac5cd01ccfa0fde32685d48779017f7c1e002e4c
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-03-19 13:05:43 +0100

    add: task 3

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
 -- Task 3
 
 -- lists all band with 'Glam rock' as their main style, ranked by their longevity
-SELECT band_name, (IFNULL(split, YEAR(CURDATE())) - formed) as lifespan FROM metal_bands WHERE INSTR(style, 'Glam rock') > 0 ORDER BY lifespan DESC
+SELECT band_name, (IFNULL(split, YEAR(CURDATE())) - formed) AS lifespan FROM metal_bands WHERE style LIKE "%Glam rock%" ORDER BY lifespan DESC;
