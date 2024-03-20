8841c83edb1dad4467f9e09d412341588a0a40c2
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-03-20 15:12:49 +0100

    fix: task 7

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -5,20 +5,20 @@
 
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN input_user_id INT)
 BEGIN
 
-DECLARE nbr_of_score INT;
-DECLARE total_score INT;
+DECLARE nbr_of_score FLOAT;
+DECLARE total_score FLOAT;
 
-SELECT COUNT(*) INTO nbr_of_score FROM corrections WHERE user_id = user_id;
-
+SELECT COUNT(*) INTO nbr_of_score FROM corrections WHERE user_id = input_user_id;
+SELECT 'nbr_of_score', nbr_of_score;
 IF nbr_of_score != 0 THEN
 
-SELECT SUM(score) INTO total_score FROM corrections WHERE user_id = user_id ;
+SELECT SUM(score) INTO total_score FROM corrections WHERE user_id = input_user_id ;
 
 
-UPDATE users SET average_score = total_score / nbr_of_score WHERE id = user_id;
+UPDATE users SET average_score = total_score / nbr_of_score WHERE id = input_user_id;
 
 
 END IF ;
