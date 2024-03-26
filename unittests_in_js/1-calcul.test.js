e3b80246cb0470ea1a97d243ce0cc758842bda9a
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-03-26 12:57:21 +0100

    fix: task 1

--- unittests_in_js/1-calcul.test.js
+++ unittests_in_js/1-calcul.test.js
@@ -1,50 +1,110 @@
 const assert = require("assert");
-const calculateNumber = require("./1-calcul");
+const calculateNumber = require("./1-calcul.js");
 
-describe("calculateNumber", function () {
-  it("Checks 1 + 3", function () {
-    assert.equal(calculateNumber("SUM", 1, 3), 4);
+describe("Case type is 'SUM'", function () {
+  it("add two positive integers", function () {
+    assert.equal(calculateNumber("SUM", 1, 2), 3);
   });
-  it("Checks 1 + 3.7", function () {
-    assert.equal(calculateNumber("SUM", 1, 3.7), 5);
+
+  it("add two positive floats", function () {
+    assert.equal(calculateNumber("SUM", 2.3, 4.6), 7);
   });
-  it("Checks 1.2 + 3.7", function () {
-    assert.equal(calculateNumber("SUM", 1.2, 3.7), 5);
+
+  it("add one positive integer and one negative integer", function () {
+    assert.equal(calculateNumber("SUM", 2, -1), 1);
   });
-  it("Checks 1.4 + 4.5", function () {
-    assert.equal(calculateNumber("SUM", 1.4, 4.5), 6);
+
+  it("add one positive integer and one negative float", function () {
+    assert.equal(calculateNumber("SUM", 1, -2.4), -1);
   });
-  it("Checks -3 + 1.2", function () {
-    assert.equal(calculateNumber("SUM", -3, 1.2), -2);
+
+  it("add one negative integer and one positive float", function () {
+    assert.equal(calculateNumber("SUM", -1, 2.4), 1);
   });
-  it("Checks 3 - 1", function () {
-    assert.equal(calculateNumber("SUBTRACT", 3, 1), 2);
+
+  it("add two negative floats", function () {
+    assert.equal(calculateNumber("SUM", -1.5, -2.4), -3);
   });
-  it("Checks 1.4 - 4.5", function () {
-    assert.equal(calculateNumber("SUBTRACT", 1.4, 4.5), -4);
+
+  it("round to the supp integer", function () {
+    assert.equal(calculateNumber("SUM", 1.5, 2.5), 5);
   });
-  it("Checks 1.2 - 3.7", function () {
-    assert.equal(calculateNumber("SUBTRACT", 1.2, 3.7), -3);
+});
+
+describe("Case type is 'SUBTRACT'", function () {
+  it("subtract two positive integers", function () {
+    assert.equal(calculateNumber("SUBTRACT", 1, 2), 1);
   });
-  it("Checks 3.7 - 1.5", function () {
-    assert.equal(calculateNumber("SUBTRACT", 3.7, 1.5), 2);
+
+  it("subtract two positive floats", function () {
+    assert.equal(calculateNumber("SUBTRACT", 2.3, 4.6), 3);
   });
-  it("Checks 0.1 - 0.1", function () {
-    assert.equal(calculateNumber("SUBTRACT", 0.1, 0.1), 0);
+
+  it("subtract one positive integer and one negative integer", function () {
+    assert.equal(calculateNumber("SUBTRACT", 2, -1), -3);
   });
-  it("Checks 3 / 1", function () {
-    assert.equal(calculateNumber("DIVIDE", 3, 1), 3);
+
+  it("subtract one positive integer and one negative float", function () {
+    assert.equal(calculateNumber("SUBTRACT", 1, -2.4), -3);
   });
-  it("Checks 3.5 / 2.1", function () {
-    assert.equal(calculateNumber("DIVIDE", 3.5, 2.1), 2);
+
+  it("subtract one negative integer and one positive float", function () {
+    assert.equal(calculateNumber("SUBTRACT", -1, 2.4), 3);
   });
-  it("Checks 1.4 / 4.5", function () {
-    assert.equal(calculateNumber("DIVIDE", 1.4, 4.5), 0.2);
+
+  it("subtract two negative floats", function () {
+    assert.equal(calculateNumber("SUBTRACT", -1.5, -2.4), -1);
   });
-  it("Checks 1.4 / 0", function () {
-    assert.equal(calculateNumber("DIVIDE", 1.4, 0), "Error");
+
+  it("subtract same number", function () {
+    assert.equal(calculateNumber("SUBTRACT", 1.5, 1.5), 0);
   });
-  it("Checks 0.1 / 0.1", function () {
-    assert.equal(calculateNumber("DIVIDE", 0.1, 0.1), "Error");
+
+  it("round to the supp integer", function () {
+    assert.equal(calculateNumber("SUBTRACT", 1.5, 2.5), 1);
+  });
+});
+
+describe("Case type is 'DIVIDE'", function () {
+  it("divide two positive integers", function () {
+    assert.equal(calculateNumber("DIVIDE", 1, 2), 0.5);
+  });
+
+  it("divide two positive floats", function () {
+    assert.equal(calculateNumber("DIVIDE", 4.3, 1.6), 2);
+  });
+
+  it("divide one positive integer and one negative integer", function () {
+    assert.equal(calculateNumber("DIVIDE", 2, -1), -2);
+  });
+
+  it("divide one positive integer and one negative float", function () {
+    assert.equal(calculateNumber("DIVIDE", 4, -2.4), -2);
+  });
+
+  it("divide one negative integer and one positive float", function () {
+    assert.equal(calculateNumber("DIVIDE", -6, 2.7), -2);
+  });
+
+  it("divide two negative floats", function () {
+    assert.equal(calculateNumber("DIVIDE", -1.5, -2.4), 0.5);
+  });
+
+  it("round to the supp integer", function () {
+    assert.equal(calculateNumber("DIVIDE", 1.5, 1.5), 1);
+  });
+
+  it("divide when round b is equal to 0", function () {
+    assert.equal(calculateNumber("DIVIDE", 4, 0.4), "Error");
+  });
+
+  it("divide when b is equal to 0", function () {
+    assert.equal(calculateNumber("DIVIDE", 4, 0), "Error");
+  });
+});
+
+describe("Case when type is not 'SUM', 'SUBTRACT', 'DIVIDE'", function () {
+  it("case when argument type is 'SIM'", function () {
+    assert.equal(calculateNumber("SIM", 2, 3), undefined);
   });
 });
