d3e3cde282c2c706ddcb7eebb066840d59d38f8f
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-03-26 11:19:08 +0100

    fix: task 0

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,5 +1,5 @@
-const calculateNumber = require("./0-calcul.js");
 const assert = require("assert");
+const calculateNumber = require("./0-calcul.js");
 
 describe("calculateNumber", function () {
   it("add two positive integers", function () {
