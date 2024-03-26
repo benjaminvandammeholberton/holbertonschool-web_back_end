7db4ba6bf8e4d6e741b5f9e8f0bb6df04c1dca6d
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-03-26 16:56:19 +0100

    fix: task 3

--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -6,6 +6,7 @@ describe("Test sendPaymentRequestToApi", function () {
   it("call the Utils.calculateNumber function", function () {
     const spy = sinon.spy(Utils, "calculateNumber");
     sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledOnce(spy);
     sinon.assert.calledWith(spy, "SUM", 100, 20);
     spy.restore();
   });
