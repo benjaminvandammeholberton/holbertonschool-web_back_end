f1c85b3492f4e3d8ef29e5af5b4ae85e3d89911c
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-03-26 11:34:15 +0100

    fix: task 0

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,28 +1,8 @@
 const assert = require("assert");
-const calculateNumber = require("./0-calcul.js");
+const calculateNumber = require("./0-calcul");
 
 describe("calculateNumber", function () {
-  it("add two positive integers", function () {
-    assert.equal(calculateNumber(2, 2), 4);
-  });
-
-  it("add two positive floats", function () {
-    assert.equal(calculateNumber(2.3, 4.6), 7);
-  });
-
-  it("add one positive integer and one negative integer", function () {
-    assert.equal(calculateNumber(2, -1), 1);
-  });
-
-  it("add one positive integer and one negative float", function () {
-    assert.equal(calculateNumber(1, -2.4), -1);
-  });
-
-  it("add two negative floats", function () {
-    assert.equal(calculateNumber(-1.5, -2.4), -3);
-  });
-
-  it("round to the supp integer", function () {
-    assert.equal(calculateNumber(1.5, 2.5), 5);
+  it("Checks 1 + 3", function () {
+    assert.equal(calculateNumber(1, 3), 4);
   });
 });
