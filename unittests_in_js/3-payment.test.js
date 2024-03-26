00d929d86f188395aaebf0988bd5b3c8bb91babd
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-03-26 16:44:07 +0100

    add: task 4

--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -1,15 +1,12 @@
 const sinon = require("sinon");
-const Utils = require("./utils");
 const sendPaymentRequestToApi = require("./3-payment");
+const Utils = require("./utils");
 
-describe("sendPaymentRequestToApi", function () {
-  it("Validates the usage of the Utils function", function () {
-    const calculate = sinon.spy(Utils, "calculateNumber");
-
+describe("Test sendPaymentRequestToApi", function () {
+  it("call the Utils.calculateNumber function", function () {
+    const spy = sinon.spy(Utils, "calculateNumber");
     sendPaymentRequestToApi(100, 20);
-
-    calculate.restore();
-    sinon.assert.calledOnce(calculate);
-    sinon.assert.calledWith(calculate, "SUM", 100, 20);
+    sinon.assert.calledWithExactly(spy, "SUM", 100, 20);
+    spy.restore;
   });
 });
